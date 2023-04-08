3daad021d6bfc81b444e00b129559e5a72d93d58
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-04-09 02:53:09 +0530

    Time: 50 ms (55.45%), Space: 14.6 MB (77.46%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -17,13 +17,24 @@ public:
             ptr1 = ptr1->next;
             ptr2 = ptr2->next;
             
+            /*
+             * Any time they collide or reach end together without colliding 
+             * then return any one of the pointers.
+             **/
+
             if(ptr1 == ptr2) return ptr1;
             
+            /*
+             * If one of them reaches the end earlier then reuse it 
+             * by moving it to the beginning of other list.
+             * Once both of them go through reassigning, 
+             * they will be equidistant from the collision point.
+             **/
             if(ptr1 == nullptr) ptr1 = headB;
             
             if(ptr2 == nullptr) ptr2 = headA;
         }
         
-        return ptr1 ? ptr1 : ptr2;
+        return ptr1;
     }
 };

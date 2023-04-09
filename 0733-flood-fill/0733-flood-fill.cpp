336682543c61093a2db601841d3af2a07d2ff8cf
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-04-09 16:59:19 +0530

    Time: 4 ms (91.89%), Space: 16.2 MB (5.29%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -6,6 +6,7 @@ class Solution {
         int n = grid[0].size();
         return ((r >= 0) && (r < m) && (c >= 0) && (c < n));
     }
+    
     void bfs(vector<vector<int>>& grid, int r, int c, int newColor, int initColor, vector<vector<int>> &visited)
     {
         queue<pair<int, int>> q;
@@ -14,20 +15,20 @@ class Solution {
         visited[r][c] = 1;
         int di[] = {-1, 0, 1, 0};
         int dj[] = {0, 1, 0, -1};
+        
         while(!q.empty())
         {
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
-            //cout<<"pop..."<<row<<" "<<col<<endl;
+
             for(int i = 0; i < 4; i++)
             {
                 int newRow = row + di[i];
                 int newCol = col + dj[i];
-                //cout<<newRow<<" "<<newCol<<endl;
+
                 if(isValid(grid, newRow, newCol) && (grid[newRow][newCol] == initColor) && !visited[newRow][newCol])
                 {
-                    //cout<<"queuing..."<<newRow<<" "<<newCol<<endl;
                     grid[newRow][newCol] = newColor;
                     q.push({newRow, newCol});
                     visited[newRow][newCol] = 1;
@@ -56,8 +57,9 @@ class Solution {
     
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
         vector<vector<int>> visited(image.size(), vector<int>(image[0].size(), 0));
-        dfs(image, sr, sc, color, image[sr][sc], visited);
+        bfs(image, sr, sc, color, image[sr][sc], visited);
         return image;
         
     }

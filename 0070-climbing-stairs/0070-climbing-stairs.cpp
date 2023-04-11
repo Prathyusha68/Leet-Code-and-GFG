c174a9f307af43303b4c790bc3364f6407fc6f63
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-04-11 13:53:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,5 +1,12 @@
 class Solution {
-    int climbRecursive(int n, int i, vector<int> &dp)
+    enum method
+    {
+        MEMORIZATION_METHOD = 0,
+        TABULATION_METHOD = 1,
+        OPTIMIZED_METHOD = 2
+    };
+    
+    int memorization(int n, int i, vector<int> &dp)
     {
         if(i >= n)
         {
@@ -12,10 +19,10 @@ class Solution {
         if(dp[i] != -1)
             return dp[i];
         
-        return (dp[i] = (climbRecursive(n, i+1, dp) + climbRecursive(n, i+2, dp)));
+        return (dp[i] = (memorization(n, i+1, dp) + memorization(n, i+2, dp)));
     }
     
-    int climbTabulation(int n)
+    int tabulation(int n)
     {
         vector<int> dp(n+1, 0);
         dp[0] = 1;
@@ -31,11 +38,10 @@ class Solution {
     {
         int prev1 = 1;
         int prev2 = 1;
-        int curr = 1;
+
         for(int i = 2; i <= n; i++)
         {
-            int tmp = curr;
-            curr = prev1+prev2;
+            int curr = prev1+prev2;
             prev2 = prev1;
             prev1 = curr;
         }
@@ -43,8 +49,48 @@ class Solution {
     }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n, -1);
-        //return climbRecursive(n, 0, dp);
-        return optimization(n);
+       int type = 2;
+       switch(type)
+       {
+           case MEMORIZATION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N) 
+                *     (There are N states therefore at max ‘N’ new problems will be solved.)
+                *  Space Complexity : O(N) + O(N)
+                *     (We are using a recursion stack space(O(N)) and a 1D array ( O(N)).)
+                */
+               vector<int> dp(n, -1);
+               return memorization(n, 0, dp);
+               break;
+           }
+           case TABULATION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N) 
+                *     (There is one loop)
+                *  Space Complexity : O(N)
+                *     (We are using an external array of size ‘N’. Stack Space is eliminated.)
+                */
+               return tabulation(n);
+               break;
+           }
+           case OPTIMIZED_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N) 
+                *     (There is one loop)
+                *  Space Complexity : O(1)
+                *     (We are using prev1, prev2 and curr variables.)
+                */
+               return optimization(n);
+               break;
+           }
+           default:
+           {
+               cout<<"Invalid Method type"<<endl;
+           }
+       }
+       return 0;
     }
 };

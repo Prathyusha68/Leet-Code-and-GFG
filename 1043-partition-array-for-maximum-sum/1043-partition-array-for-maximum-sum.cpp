4deac4705000b56288da9679f6b9ccdfb56be3fc
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-05-06 23:31:22 +0530

    Time: 19 ms (60.00%), Space: 8.5 MB (90.42%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,128 @@
+#include<bits/stdc++.h>
+class Solution {
+    enum method
+    {
+        RECURSION_METHOD = 0,
+        MEMORIZATION_METHOD = 1,
+        TABULATION_METHOD = 2,
+        OPTIMIZED_METHOD = 3
+    };
+    
+    int recursion(int i, vector<int>& arr, int k)
+    {
+        int n = arr.size();
+        if(i == n) return 0;
+        
+        int len = 0, maxSum = INT_MIN, maxVal = INT_MIN;
+        
+        for(int j = i; j < min(i+k, n); j++)
+        {
+            len++;
+            maxVal = max(arr[j], maxVal);
+            
+            int sum = (len * maxVal) + recursion(j+1, arr, k);
+            
+            maxSum = max(maxSum, sum);            
+        }
+        
+        return maxSum;
+    }
+    
+    int memorization(int i, vector<int>& arr, int k, vector<int> &dp)
+    {
+        int n = arr.size();
+        if(i == n) return 0;
+        
+        if(dp[i] != -1) return dp[i];
+        
+        int len = 0, maxSum = INT_MIN, maxVal = INT_MIN;
+        
+        for(int j = i; j < min(i+k, n); j++)
+        {
+            len++;
+            maxVal = max(arr[j], maxVal);
+            
+            int sum = (len * maxVal) + recursion(j+1, arr, k);
+            
+            maxSum = max(maxSum, sum);            
+        }
+        
+        return dp[i] = maxSum;
+    }
+    
+    int tabulation(vector<int>& arr, int k)
+    {
+        int n = arr.size();
+        vector<int> dp(n+1, 0);
+        
+        for(int i = n-1; i >= 0; i--)
+        {
+            int len = 0, maxSum = INT_MIN, maxVal = INT_MIN;
+
+            for(int j = i; j < min(i+k, n); j++)
+            {
+                len++;
+                maxVal = max(arr[j], maxVal);
+
+                int sum = (len * maxVal) + dp[j+1];
+
+                maxSum = max(maxSum, sum); 
+            } 
+            dp[i] = maxSum;
+        }
+        return dp[0];
+    }
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+      int type = 2;
+       switch(type)
+       {
+           case RECURSION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(2^N * N) Exponential
+                *  Space Complexity : O(N)
+                *     (We are using a recursion stack space(O(N)).)
+                */
+
+               return recursion(0, arr, k);
+               break;
+           }
+           case MEMORIZATION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N * N * N) 
+                *     (There are 2 variables i and j, therefore, N*N states and we explicitly
+                *      run a loop inside the function which will run for N times, therefore at
+                *      max ‘N*N*N’ new problems will be solved.)
+                *  Space Complexity : O(N * N) + O(N)
+                *     (We are using a recursion stack space(O(N)) and a 2D array ( O(N*N)).)
+                */
+               
+               vector<int> dp(arr.size(), -1);
+               return memorization(0, arr, k, dp);
+               break;
+           }
+           case TABULATION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N * N) 
+                *     (There are 2 variables i and j, therefore, N*N states and we explicitly
+                *      run a loop inside the function which will run for N times, therefore at
+                *      max ‘N*N*N’ new problems will be solved.)
+                *  Space Complexity : O(N)
+                *     (We are using an external array of size ‘N’. Stack Space is eliminated.)
+                */
+               
+               return tabulation(arr, k);
+               break;
+           }
+           default:
+           {
+               cout<<"Invalid Method type"<<endl;
+           }
+       }
+        
+       return 0;  
+    }
+};

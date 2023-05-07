9e86deb22428174eaeef23158f619d5f6287156d
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-05-08 00:55:06 +0530

    Time: 8 ms (45.97%), Space: 8.9 MB (57.03%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,95 @@
+class Solution {
+    enum method
+    {
+        RECURSION_METHOD = 0,
+        MEMORIZATION_METHOD = 1,
+        TABULATION_METHOD = 2,
+        OPTIMIZED_METHOD = 3
+    };
+    
+    int recursion(int i, int j, int n, vector<vector<int>>& triangle)
+    {
+        if(i == n-1) return triangle[i][j];
+        
+        int down = triangle[i][j] + recursion(i+1, j, n, triangle);
+        int diagnol = triangle[i][j] + recursion(i+1, j+1, n, triangle);
+        
+        return min(down, diagnol);
+    }
+    
+    int memorization(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp)
+    {
+        if(i == n-1) return triangle[i][j];
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int down = triangle[i][j] + memorization(i+1, j, n, triangle, dp);
+        int diagnol = triangle[i][j] + memorization(i+1, j+1, n, triangle, dp);
+        
+        return dp[i][j] = min(down, diagnol);
+    }
+    
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+       int type = 1;
+       switch(type)
+       {
+           case RECURSION_METHOD :
+           {
+               /**
+                *  Time Complexity : Exponentional  
+                *  Space Complexity : O(N * M)
+                *     (We are using a recursion stack space(O(N * M)).)
+                */
+               int n = triangle.size();
+               
+               return recursion(0, 0, n, triangle);
+               break;
+           }
+           case MEMORIZATION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N * M) 
+                *     (There are N*N states therefore at max ‘N*N’ new problems will be solved.)
+                *  Space Complexity : O(N-1 * M-1) + O(N * M)
+                *     (We are using a recursion stack space(O(N * M)) and a 2D array
+                *     (O(N-1 * M-1)).)
+                */
+               int n = triangle.size();
+
+               vector<vector<int>> dp(n, vector<int>(n, -1));
+               
+               return memorization(0, 0, n, triangle, dp);
+               break;
+           }
+           case TABULATION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N * M) 
+                *     (There are two nested loops)
+                *  Space Complexity : O(N * M)
+                *     (We are using an external array of size ‘N * M’. Stack Space is
+                *      eliminated.)
+                */
+               //return tabulation(obstacleGrid);
+               break;
+           }
+           case OPTIMIZED_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N * M) 
+                *     (There are two nested loops)
+                *  Space Complexity : O(N)
+                *     (We are using an external array of size ‘N’ to store only one row.)
+                */
+               //return optimization(obstacleGrid);
+               break;
+           }
+           default:
+           {
+               cout<<"Invalid Method type"<<endl;
+           }
+       }
+       return 0;
+    }
+};

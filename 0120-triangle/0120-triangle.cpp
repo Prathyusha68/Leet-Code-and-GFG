7366ea51775cb8a032d9fdc1b046690e35fe765f
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-05-08 01:14:09 +0530

    Time: 8 ms (46.00%), Space: 9.1 MB (35.73%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -29,9 +29,33 @@ class Solution {
         return dp[i][j] = min(down, diagnol);
     }
     
+    int tabulation(vector<vector<int>>& triangle)
+    {
+        int n = triangle.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+        for(int j = 0; j < n; j++)
+            dp[n-1][j] = triangle[n-1][j];
+        
+        for(int i = n-2; i >= 0; i--)
+        {
+            for(int j = i; j >= 0; j--)
+            {
+                int down = triangle[i][j] + dp[i+1][j];
+
+                int diagnol = triangle[i][j] + dp[i+1][j+1];
+        
+                dp[i][j] = min(down, diagnol);
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-       int type = 1;
+       int type = 2;
        switch(type)
        {
            case RECURSION_METHOD :
@@ -71,7 +95,7 @@ public:
                 *     (We are using an external array of size ‘N * M’. Stack Space is
                 *      eliminated.)
                 */
-               //return tabulation(obstacleGrid);
+               return tabulation(triangle);
                break;
            }
            case OPTIMIZED_METHOD :

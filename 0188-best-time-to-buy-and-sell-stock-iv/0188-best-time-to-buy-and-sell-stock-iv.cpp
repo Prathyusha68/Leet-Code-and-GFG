93406e18acad68c46b5e175dfd11bfb1c05a88b9
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-05-08 21:37:41 +0530

    Time: 25 ms (47.03%), Space: 13.9 MB (55.86%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,105 @@
+class Solution {
+     enum method
+    {
+        RECURSION_METHOD = 0,
+        MEMORIZATION_METHOD = 1,
+        TABULATION_METHOD = 2,
+        OPTIMIZED_METHOD = 3
+    };
+    
+    int recursion(int ind, int transactions, int k, vector<int>& prices)
+    {
+        if(transactions == 2*k) return 0;
+        
+        if(ind == prices.size()) return 0;
+        
+        if(transactions%2 == 0)
+        {
+            return max((-prices[ind] + recursion(ind+1, transactions+1, k, prices)),
+                      (0 + recursion(ind+1, transactions, k, prices)));
+        }
+                       
+        return max((prices[ind] + recursion(ind+1, transactions+1, k, prices)),
+                  (0 + recursion(ind+1, transactions, k, prices)));
+    }
+     
+    int memorization(int ind, int transactions, int k, vector<int>& prices, vector<vector<int>> &dp)
+    {
+        if(transactions == 2*k) return 0;
+        
+        if(ind == prices.size()) return 0;
+        
+        if(dp[ind][transactions] != -1) return dp[ind][transactions];
+
+        if(transactions%2 == 0)
+        {
+            return dp[ind][transactions] = max((-prices[ind] + memorization(ind+1, transactions+1, k, prices, dp)),
+                      (0 + memorization(ind+1, transactions, k, prices, dp)));
+        }
+          
+        return dp[ind][transactions] = max((prices[ind] + memorization(ind+1, transactions+1, k, prices, dp)), (0 + memorization(ind+1, transactions, k, prices, dp)));
+    }
+    
+public:
+    int maxProfit(int k, vector<int>& prices) {
+       int type = 1;
+       switch(type)
+       {
+           case RECURSION_METHOD :
+           {
+               /**
+                *  Time Complexity : Exponentional  
+                *  Space Complexity : O(N)
+                *     (We are using a recursion stack space(O(N)).)
+                */
+               
+               return recursion(0, 0, k, prices);
+               break;
+           }
+           case MEMORIZATION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N * 2) 
+                *     (There are N*2 states therefore at max ‘N*2’ new problems will be solved.)
+                *  Space Complexity : O(N) + O(N * 2)
+                *     (We are using a recursion stack space(O(N)) and a 2D array
+                *     (O(N * 2)).)
+                */
+               int n = prices.size();
+
+               vector<vector<int>> dp(n+1, vector<int>(2*k, -1));
+               
+               return memorization(0, 0, k, prices, dp);
+               break;
+           }
+           case TABULATION_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N * 2) 
+                *     (There are two nested loops)
+                *  Space Complexity : O(N * 2)
+                *     (We are using an external array of size ‘N * 2’. Stack Space is
+                *      eliminated.)
+                */
+               //return tabulation(prices);
+               break;
+           }
+           case OPTIMIZED_METHOD :
+           {
+               /**
+                *  Time Complexity : O(N * 2) 
+                *     (There are two nested loops)
+                *  Space Complexity : O(1)
+                *     (We are using an external array of size ‘2’ to store only one row.)
+                */
+               //return optimization(prices);
+               break;
+           }
+           default:
+           {
+               cout<<"Invalid Method type"<<endl;
+           }
+       }
+       return 0;
+    }
+};

1a7cf265b4b3f5ddee15f6aa25a0d5c81a94de9a
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-05-15 01:14:40 +0530

    Time: 21 ms (74.07%), Space: 13.8 MB (54.60%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,57 @@
+class Solution {
+private:
+    void toposort(int K, vector<int> adjList[], vector<int> &topo)
+    {
+        int inDegree[K+1];
+        memset(inDegree, 0, sizeof(inDegree));
+        
+        for(int i = 0; i < K; i++)
+        {
+            for(auto it : adjList[i])
+            {
+                inDegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i = 0; i < K; i++)
+        {
+            if(inDegree[i] == 0)
+                q.push(i);
+        }
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            topo.push_back(node);
+            q.pop();
+            
+            for(auto it : adjList[node])
+            {
+                inDegree[it]--;
+                if(inDegree[it] == 0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+    }
+    
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adjList[numCourses];
+        vector<int> topo;
+        
+        for(auto it: prerequisites)
+        {
+            adjList[it[1]].push_back(it[0]);            
+        }
+        
+        toposort(numCourses, adjList, topo);
+
+        if(topo.size() != numCourses)
+            return false;
+        
+        return true;
+    }
+};

df502f0b509c62173b3e2e198c116cc7f2c71bd9
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-06-02 00:51:57 +0530

    Time: 63 ms (12.98%), Space: 26.1 MB (63.06%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,39 +11,25 @@
  */
 class Solution {
 private:
-    TreeNode* buildTree(vector<int>& preorder, int pStart, int pEnd,
-                        vector<int>& inorder, int iStart, int iEnd, map<int, int> mp) {
-        if((pStart > pEnd) || (iStart > iEnd))
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder,
+                    int& rootIdx, int left, int right)
+    {
+        if (left > right)
             return NULL;
         
-        TreeNode* root = new TreeNode(preorder[pStart]);
-        
-        int rootAt = mp[root->val];
-        int numElem = rootAt-iStart;
-        
-        root->left = buildTree(preorder, pStart+1, pStart+numElem,
-                         inorder, iStart, rootAt-1, mp);
-        root->right = buildTree(preorder, pStart+numElem+1, pEnd,
-                         inorder, rootAt+1, iEnd, mp);
-        
-        return root;
-    }
-    
-public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int rootIdx = 0;
-        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
-    }
-    
-    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& rootIdx, int left, int right) {
-        if (left > right) return NULL;
         int pivot = left;  // find the root from inorder
-        while(inorder[pivot] != preorder[rootIdx]) pivot++;
+        while(inorder[pivot] != preorder[rootIdx])
+            pivot++;
         
         rootIdx++;
         TreeNode* newNode = new TreeNode(inorder[pivot]);
         newNode->left = build(preorder, inorder, rootIdx, left, pivot-1);
         newNode->right = build(preorder, inorder, rootIdx, pivot+1, right);
         return newNode;
+    }
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int rootIdx = 0;
+        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
     }
 };

31a5cd9b7e94b5d9aa9d357ec6dd56ba1020191d
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-06-06 02:44:55 +0530

    Time: 35 ms (31.44%), Space: 27 MB (79.31%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -9,10 +9,18 @@ public:
                 if(++cnt > 1)
                     return false;
                 
+                // check case "5, 7, 1"
+                // if prev elem > next elem we can't change curr elem
+                // So, we need to curr elem as next elem
                 if((i-1 >= 0) && (nums[i-1] > nums[i+1]))
                     nums[i+1] = nums[i];
                 else
+                {
+                    // check case "7, 1, 8"
+                    // if next elem is greater than prev elem 
+                    // we need to take curr elem as next elem
                     nums[i] = nums[i+1];
+                }
             }
         }
         

4f49d3f26a2cd27d028e066d2dd6d6ae0f5e173d
Prathyusha Jakka <86401782+Prathyusha68@users.noreply.github.com>
2023-06-08 16:41:10 +0530

    Time: 4 ms (41.69%), Space: 6.2 MB (92.77%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int right = 0, left = 0;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(')
+                right++;
+            else if(right > 0)
+                right--;
+            else
+                left++;            
+        }
+        
+        return left+right;
+    }
+};
